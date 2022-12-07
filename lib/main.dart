9433ee4cc5eddd3e176517e2d7dc9f3e759a1671
Victor Bonanho <contato@victorbonanho.com>
2022-12-06 23:44:00 -0300

    Starting run the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,15 @@
+//Flutter is a bunch of widgets
+
+// ignore_for_file: use_key_in_widget_constructors, prefer_const_constructors
+
+import 'package:flutter/material.dart';
+
 void main() {}
+
+class MyApp extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Text('Hello'),
+    );
+  }
+}
